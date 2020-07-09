476a70b3b71d377c4f91efb35f02372d362dc4b7
AngryFennec <shellfix@gmail.com>
2020-07-09 15:35:03 +0300

    Параллакс jstick

--- source/js/main.js
+++ source/js/main.js
@@ -106,6 +106,19 @@ const config = {
   },
 
 };
-
+if (document.querySelector('.app')) {
 const r = window.rolly(config);
 r.init();
+}
+
+document.addEventListener("mousemove", parallax);
+  const elem = document.querySelector(".main-header__para");
+
+  function parallax(e) {
+      let _w = window.innerWidth/2;
+      let _h = window.innerHeight/2;
+      let _mouseX = e.clientX;
+      let _depth = `${50 - (_mouseX - _w) * 0.01}%`;
+      let x = `${_depth}`;
+      elem.style.backgroundPosition = x;
+  }
